56ee429afe061881edabb9e5a0b62238b49442aa
Night <dranhata@gmail.com>
2024-04-16 13:48:42 +0700

    Create user and role

--- book-network/src/main/java/com/alibou/book/BookNetworkApplication.java
+++ book-network/src/main/java/com/alibou/book/BookNetworkApplication.java
@@ -2,8 +2,10 @@ package com.alibou.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApplication {
 
 	public static void main(String[] args) {
