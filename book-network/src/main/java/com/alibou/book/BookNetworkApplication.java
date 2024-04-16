4967ecef271f1ee0c6837e3883bd1f3b1387fdfc
Night <dranhata@gmail.com>
2024-04-16 13:16:39 +0700

    first commit

---
+++ book-network/src/main/java/com/alibou/book/BookNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApplication.class, args);
+	}
+
+}
---
+++ book-network/src/test/java/com/alibou/book/BookNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
