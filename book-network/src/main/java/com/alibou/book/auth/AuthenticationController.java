46d1d49b0a4458f6367fe410a31acf9011fa14c6
Night <dranhata@gmail.com>
2024-04-18 00:06:50 +0700

    implement Activate account function

--- book-network/src/main/java/com/alibou/book/auth/AuthenticationController.java
+++ book-network/src/main/java/com/alibou/book/auth/AuthenticationController.java
@@ -31,4 +31,11 @@ public class AuthenticationController {
     ) {
         return ResponseEntity.ok(service.authenticate(request));
     }
+
+    @GetMapping("/activate-account")
+    public void confirm(
+            @RequestParam String token
+    ) throws MessagingException {
+        service.activateAccount(token);
+    }
 }
