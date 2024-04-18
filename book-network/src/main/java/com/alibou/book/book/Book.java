270f399844df9ef94606b4b91193086730e0933a
Night <dranhata@gmail.com>
2024-04-18 09:30:36 +0700

    Create Book, Feedback entities

---
+++ book-network/src/main/java/com/alibou/book/book/Book.java
@@ -0,0 +1,27 @@
+package com.alibou.book.book;
+
+import com.alibou.book.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+}
---
+++ book-network/src/main/java/com/alibou/book/feedback/Feedback.java
@@ -0,0 +1,22 @@
+package com.alibou.book.feedback;
+
+import com.alibou.book.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Integer note;
+    private String comment;
+
+}
