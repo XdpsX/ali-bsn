37c0fb6f4dd2066f8b514468b271da39414c7e11
Night <dranhata@gmail.com>
2024-07-04 22:07:21 +0700

    CI/CD Pipeline

--- book-network/src/main/java/com/alibou/book/config/BeansConfig.java
+++ book-network/src/main/java/com/alibou/book/config/BeansConfig.java
@@ -1,6 +1,7 @@
 package com.alibou.book.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -25,6 +26,9 @@ import java.util.List;
 public class BeansConfig {
 
     private final UserDetailsService userDetailsService;
+
+    @Value("${application.cors.origins:*}") // not recommend *
+    private List<String> allowedOrigins;
 
     @Bean
     public AuditorAware<Integer> auditorAware() {
@@ -53,21 +57,10 @@ public class BeansConfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:8080"));
-        config.setAllowedHeaders(Arrays.asList(
-                HttpHeaders.ORIGIN,
-                HttpHeaders.CONTENT_TYPE,
-                HttpHeaders.ACCEPT,
-                HttpHeaders.AUTHORIZATION
-        ));
-        config.setAllowedMethods(Arrays.asList(
-                "GET",
-                "POST",
-                "DELETE",
-                "PUT",
-                "PATCH"
-        ));
+//        config.setAllowCredentials(true); // not implement when origins = *
+        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedHeaders(Arrays.asList("*")); // not recommend for prod
+        config.setAllowedMethods(Arrays.asList("*")); // not recommend for prod
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
 
