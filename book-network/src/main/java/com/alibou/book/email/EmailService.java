fc1ff47afe5ca4d4abe3233a09ece1b344cd254f
Night <dranhata@gmail.com>
2024-07-04 10:27:38 +0700

    Dockerizing

--- book-network/src/main/java/com/alibou/book/email/EmailService.java
+++ book-network/src/main/java/com/alibou/book/email/EmailService.java
@@ -34,7 +34,7 @@ public class EmailService {
         if (emailTemplate == null){
             templateName = "confirm-email";
         }else {
-            templateName = emailTemplate.name();
+            templateName = emailTemplate.getName();
         }
 
         MimeMessage mimeMessage = mailSender.createMimeMessage();
